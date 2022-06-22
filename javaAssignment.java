5be3a1c47612e170ee774dd0e43c26ce0a18d7f9
tharunichitti <90306610+tharunichitti@users.noreply.github.com>
2022-06-22 10:54:38 +0530

    Update javaAssignment.java

--- javaAssignment.java
+++ javaAssignment.java
@@ -1,133 +1,89 @@
 package javaAssignment;
 
-
 import java.io.File;
 import java.io.FileInputStream;
 import java.io.IOException;
 import java.util.HashSet;
 import java.util.Scanner;
 import java.util.Set;
+
 import org.apache.pdfbox.pdmodel.PDDocument;
 import org.apache.pdfbox.text.PDFTextStripper;
 import org.apache.poi.xwpf.extractor.XWPFWordExtractor;
 import org.apache.poi.xwpf.usermodel.XWPFDocument;
 
-public class javaAssignment {
+public class pdfReader {
+	public static void main(String[] args) throws IOException {
+		File f = new File("C:\\Users\\TGODUGU\\Documents\\resume");
+File files[] = f.listFiles();
+		
+		System.out.println("--------------------------------------------------------------------------------------");
+		System.out.println("|                     Total number of files  in ::" + files.length+"                                  |");
+		System.out.println("--------------------------------------------------------------------------------------");
+		System.out.println("|                     Number Of Keyword Want To Enter                                |");
+		System.out.println("--------------------------------------------------------------------------------------");
+		Scanner sc = new Scanner(System.in);
+		int numOfKeys = sc.nextInt();
+		 sc.nextLine();
+		Set<String> set = new HashSet<String>();
+		System.out.println("--------------------------------------------------------------------------------------");
+		System.out.println("|                     Please Enter The Keyword                                       |");
+		System.out.println("--------------------------------------------------------------------------------------");
+		for (int i = 0; i < numOfKeys; i++) {
+			String strting = sc.nextLine();
+			set.add(strting);
+		}
+		System.out.println("----------------------------------------------------------------------------------");
+		System.out.println("|                              SHOWING RESULTS                                    |");
+		System.out.println("----------------------------------------------------------------------------------");
+		System.out.println("|                 FILE NAME                |              PERCENTAGE %            |");
+		System.out.println("----------------------------------------------------------------------------------");
+		for (File file : files) {
+			int count=0;
+			if (file.getName().contains("docx")) {				
+				FileInputStream input = new FileInputStream(file);
+				XWPFDocument document = new XWPFDocument(input);
+				XWPFWordExtractor ex = new XWPFWordExtractor(document);
+				String str = ex.getText().toLowerCase();
+                for(String word:set) { 
+				if (str.contains(word)) {
+					count++;
+				}}
+                if(count>0) {
+                	float percentage=(float) percentage(count,numOfKeys);
+                	System.out.printf("%-44s"+"|"+"%.2f",file.getName(),percentage);
+                	System.out.println("");
+                }
 
-public static void main(String[] args) throws IOException {
-// TODO Auto-generated method stub
+			} else if (file.getName().contains("pdf")) {
+				
+				PDDocument document = PDDocument.load(file);
+				PDFTextStripper test = new PDFTextStripper();
+				String str = test.getText(document).toLowerCase();
+			    for(String word:set) { 
+					if (str.contains(word)) {
+						count++;
+					}}
+			    if(count>0) {
+                	float percentage=(float) percentage(count,numOfKeys);
+                	System.out.printf("%-44s"+"|"+"%.2f",file.getName(),percentage);
+                	System.out.println("");
+                	
+                }
+				document.close();
 
+			}
 
+		}
 
-String filepath = "C:\\Users\\TGODUGU\\Documents\\resume";
+	}
 
-
-
-File ip = new File(filepath);
-
-
-
-File[] fp = ip.listFiles();
-
-
-
-Set<String> set = new HashSet<String>();
-
-
-
-Scanner input = new Scanner(System.in);
-System.out.println("Enter the number of Keywords required to compare :");
-int numOfKey = input.nextInt();
-// System.out.println(numOfKey);
-input.nextLine();
-System.out.println("Enter the keywords :");
-for (int i = 0; i < numOfKey; i++) {
-String keyword = input.nextLine();
-set.add(keyword);
-}
-System.out.println("List of keywords are : ");
-System.out.println(set);
-System.out.println(" ");
-System.out.println("percentages of files containing Keywords are : ");
-
-
-
-System.out.println(" ");
-
-
-
-for (File file : fp) {
-
-
-
-if (file.getName().contains("docx")) {
-FileInputStream one = new FileInputStream(file);
-XWPFDocument doc = new XWPFDocument(one);
-XWPFWordExtractor ex = new XWPFWordExtractor(doc);
-String resume = ex.getText();
-ex.close();
-String Resume = resume.toLowerCase();
-int count = 0;
-for (String ele : set) {
-if (Resume.contains(ele)) {
-
-
-
-count++;
-}
-}
-System.out.println(file.getName());
-percentage(count, numOfKey);
-
-
-
-} else {
-PDDocument document = PDDocument.load(file);
-PDFTextStripper test = new PDFTextStripper();
-String resume = test.getText(document);
-String Resume = resume.toLowerCase();
-int count = 0;
-for (String ele : set) {
-if (Resume.contains(ele)) {
-
-count++;
-}
-}
-System.out.println(file.getName());
-percentage(count, numOfKey);
-document.close();
-}
-System.out.println("*********************************************************");
+	private static double percentage(double count,double num) {
+		double per;
+		per=(count/num)*100;
+		return per;
 
 
-
-}
-
-
-
-}
-
-
-
-public static void percentage(int count, int num) {
-if (count == 0) {
-System.out.println("0%");
-} else {
-
-
-
-System.out.println("matched keywords count is " + count);
-// System.out.println(file.getName());
-double diff = num - count;
-double per = (diff / num) * 100;
-double matched = 100 - per;
-System.out.println(String.format("%.2f", matched));
-
-
-
-}
-
-
-
-}
-}
+		
+			}
+	}
